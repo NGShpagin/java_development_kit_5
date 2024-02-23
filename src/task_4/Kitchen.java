0b20adc9cf08de5553680b5c6a9092cdfc62514d
Nikolay <shpagin9393@mail.ru>
2024-02-23 10:42:26 +0300

    first init

---
+++ src/task_4/Kitchen.java
@@ -0,0 +1,11 @@
+package task_4;
+
+import task3.Runner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Kitchen {
+    public boolean isFree = true;
+}
